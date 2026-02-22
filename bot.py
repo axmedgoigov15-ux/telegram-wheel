c9f0109cfb956f948147d3e8e488a1d5742f70de
axmedgoigov15-ux <axmed.goigov15@gmail.com>
2026-02-23 00:52:58 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,31 +2,13 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
 
 BOT_TOKEN = "8321012082:AAGjcOJK9Z4AGda-mf_mKjCIpjP5c5f86jw"
-ADMIN_ID = 8142916139
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
 
 @dp.message_handler(commands=["start"])
 async def start(message: types.Message):
-    kb = types.InlineKeyboardMarkup()
-    kb.add(
-        types.InlineKeyboardButton(
-            text="🎰 Крутить колесо",
-            web_app=types.WebAppInfo(
-                url="https://axmedgoigov15-ux.github.io/telegram-wheel/"
-            )
-        )
-    )
-    await message.answer("Бот запущен ✅", reply_markup=kb)
-
-@dp.message_handler(content_types=types.ContentType.WEB_APP_DATA)
-async def webapp(message: types.Message):
-    await bot.send_message(
-        ADMIN_ID,
-        f"Данные из WebApp:\n{message.web_app_data.data}"
-    )
-    await message.answer("Результат получен 👍")
+    await message.answer("✅ Бот успешно запущен")
 
 if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True)
+    executor.start_polling(dp)
